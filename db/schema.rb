a7d8fdcfd4ed5b2adb81257622db0785a97cc476
Graziele Vasconcelos <grazisbvasconcelos@gmail.com>
2021-10-05 10:17:29 -0300

    Criando migrate para position applicant company

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_05_020150) do
+ActiveRecord::Schema.define(version: 2021_10_05_131543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "applicants", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone"
+    t.bigint "user_id", null: false
+    t.bigint "position_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["position_id"], name: "index_applicants_on_position_id"
+    t.index ["user_id"], name: "index_applicants_on_user_id"
+  end
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.string "url"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_companies_on_user_id"
+  end
+
+  create_table "positions", force: :cascade do |t|
+    t.string "name"
+    t.integer "career"
+    t.integer "contract"
+    t.boolean "remote"
+    t.string "city"
+    t.string "state"
+    t.text "summary"
+    t.text "description"
+    t.boolean "publish"
+    t.bigint "company_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["company_id"], name: "index_positions_on_company_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +64,8 @@ ActiveRecord::Schema.define(version: 2021_10_05_020150) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "applicants", "positions"
+  add_foreign_key "applicants", "users"
+  add_foreign_key "companies", "users"
+  add_foreign_key "positions", "companies"
 end
